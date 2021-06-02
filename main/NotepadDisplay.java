bc70a454a5ec20f03a14a3242589d1f82ac1057b
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-06-02 18:44:56 +0100

    Create initial constructor for NotepadDisplay

--- main/NotepadDisplay.java
+++ main/NotepadDisplay.java
@@ -1,8 +1,9 @@
 package main;
 
 import javax.swing.*;
+import java.awt.*;
 
-public class NotepadDisplay {
+public class NotepadDisplay extends JFrame {
     private JPanel panel1;
     private JPanel menuBarPanel;
     private JPanel buttonsPanel;
@@ -14,4 +15,24 @@ public class NotepadDisplay {
     private JTextArea textArea1;
     private JPanel titlepanel;
     private JLabel lblTitle;
+
+    private final String noFileText = "No file selected.";
+
+    private final Color disabledTextAreaColor = Color.LIGHT_GRAY;
+
+    public NotepadDisplay() {
+        JFrame frame = NotepadDisplay.this;
+        frame.add(panel1);
+        frame.setSize(400, 400);
+        frame.setDefaultCloseOperation(DISPOSE_ON_CLOSE);
+        frame.setVisible(true);
+
+        lblTitle.setText(noFileText);
+        textArea1.setText(noFileText);
+
+        btnSaveFile.setEnabled(false);
+        textArea1.setEnabled(false);
+        textArea1.setBackground(disabledTextAreaColor);
+        textArea1.setDisabledTextColor(Color.BLACK);
+    }
 }
