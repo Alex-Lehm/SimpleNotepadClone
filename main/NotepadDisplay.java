4a19261541892021a0f634f68486d4e2c6c0a15e
Alexander Lehm <A.R.Lehm2@ncl.ac.uk>
2021-06-02 16:26:07 +0100

    Create UI form design, ready for implementation

---
+++ main/NotepadDisplay.java
@@ -0,0 +1,17 @@
+package main;
+
+import javax.swing.*;
+
+public class NotepadDisplay {
+    private JPanel panel1;
+    private JPanel menuBarPanel;
+    private JPanel buttonsPanel;
+    private JButton btnNewWindow;
+    private JButton btnNewFile;
+    private JButton btnSaveFile;
+    private JButton btnOpenFile;
+    private JPanel mainPanel;
+    private JTextArea textArea1;
+    private JPanel titlepanel;
+    private JLabel lblTitle;
+}
